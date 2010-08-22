9d3097fd85752306d5c5fbb7abece8a323b3b390
Richard Lyman <richard.lyman@gmail.com>
2010-08-21 22:36:46 -0600

    Refactoring markdown grammar. Modifications of markdown tests.

--- src/com/lithinos/amotoen/markdown.clj
+++ src/com/lithinos/amotoen/markdown.clj
@@ -9,20 +9,24 @@
 (ns com.lithinos.amotoen.markdown)
 
 (def grammar {
-    :Start                          :Document
-    :Enter                          (System/getProperty "line.separator")
-    :Line                           ['(+ (| :EscapedChar :Span #"^.")) '(| :Enter :$)]
-    :Document                       ['(+ (| :BlankLine :Block :Line)) :$]
+    :Start      :Document
+    :Document   ['(+ :LinePart) :$]
+    :LinePart       '(| :EscapedChar :Span :RegularChar)
+    :RegularChar    #"^(?s)."
     :EscapedChar                    ["\\" :EscapableChar]
         :EscapableChar              '(| "\\" "`" "*" "_" "{" "}" "[" "]" "(" ")" "#" "+" "-" "." "!")
-    :Span                           '(| :Link :LiteralAsterisk :LiteralUnderscore :Emphasis :Code :Image :AutomaticLink)
+    ; Done - as long as regexs like :LinkTextOrLabel consume chars like newline
+    :Span                           '(| :Link :LiteralAsterisk :LiteralUnderscore :Emphasis :Code :Image :AutomaticLink :BlankLine)
+        ; Done
         :LiteralAsterisk            "* "
         :LiteralUnderscore          "_ "
+        ; Done
         :Link                       ["[" :LinkTextOrLabel "]" '(| :InlineLink :ReferenceLink)]
             :LinkTextOrLabel        #"^[^]]+"
             :InlineLink             ["(" :LinkURL '(? [" " "\"" :LinkTitle "\""]) ")"]
             :ReferenceLink          '(| :ImplicitRefLink ['(? " ") "[" :LinkLabel "]"])
                :ImplicitRefLink     ['(? " ") "[]"]
+        ; Done
         :Emphasis                   '(| :StrongEmphasis :EMEmphasis)
             :StrongEmphasis         '(| :StrongEmphasis** :StrongEmphasis__)
                 :StrongEmphasis**   ["**" '(+ [(! "**") #"^(?s)."]) "**"]
@@ -30,46 +34,83 @@
             :EMEmphasis             '(| :EMEmphasis* :EMEmphasis_)
                 :EMEmphasis*        ["*" #"^[^*]+" "*"]
                 :EMEmphasis_        ["_" #"^[^_]+" "_"]
-        :Code                       '(| :SingleBacktick :DoubleBacktick :SpacedBacktick)
+        ; Done-ish
+        :Code                       '(| :SingleBacktick :DoubleBacktick); :SpacedBacktick)
             :SingleBacktick         ["`" '(? " ") #"^[^`]+" '(? " ") "`"]
             :DoubleBacktick         ["``" '(? " ") '(+ [(! "``") #"^(?s)."]) '(? " ") "``"]
+        ; Done
         :Image                      ["!" :Link]
+        ; Done
         :AutomaticLink              ["<" :LinkText ">"]
             :LinkText               #"^[^>]+"
-    :Block                          '(| :Paragraph :Header :Blockquote :List :Codeblock :HRule :LinkLabelDefinition)
-        :Paragraph                  ['(+ :LineOfText) '(& (| :BlankLine :$))]
-            :BlankLine              [#"^[ \t]*$" :Enter]
-            :LineOfText             '(| :BreakEndedLine :Line)
-                :BreakEndedLine     ['(& #"^.+[ ][ ]+$") :Line]
-        :Header                     '(| :SetextHeader :ATXHeader)
-            :SetextHeader           [:HeaderText :Enter '(| (+ "=") (+ "-"))]
-            :ATXHeader              '(| :ATXHeader1 :ATXHeader2 :ATXHeader3 :ATXHeader4 :ATXHeader5 :ATXHeader6)
-                :ATXHeader1         ["#"        :HeaderText '(* "#")]
-                :ATXHeader2         ["##"       :HeaderText '(* "#")]
-                :ATXHeader3         ["###"      :HeaderText '(* "#")]
-                :ATXHeader4         ["####"     :HeaderText '(* "#")]
-                :ATXHeader5         ["#####"    :HeaderText '(* "#")]
-                :ATXHeader6         ["######"   :HeaderText '(* "#")]
-        :Blockquote                 [:BlockquoteFirstLine '(* (| :BlockquoteFirstLine :Line))]
-            :BlockquoteFirstLine    ['(+ "> ") :Line]
-        :List                       '(| :OrderedList :UnorderedList)
-            :OrderedList            '(+ [:OrderedMarker     :ListLine])
-            :UnorderedList          '(+ [:UnorderedMarker   :ListLine])
-                :OrderedMarker      [:ListIndent #"^\d+\."          :ListSpace]
-                :UnorderedMarker    [:ListIndent '(| "*" "+" "-")   :ListSpace]
-                :ListIndent         #"^[ ]{0,3}"
-                :ListSpace          '(| #"^[ ]+" "\t")
-                :ListLine           :Line
-        :Codeblock                  [:CodeIndent :Line]
-            :CodeIndent             '(| "    " "\t")
-        :HRule                      '(| #"^(-|- ){3,}" #"^(\*|\* ){3,}" #"^(_|_ ){3,}")
-        :LinkLabelDefinition        [#"^[ ]{0,3}" "[" :LinkLabel "]:" #"^[ \t]+" :LinkLabelURL '(? (| :SingleLineLLDTitle :DblLineLLDTitle))]
-            :LinkLabel              #"^[^]]+"
-            :LinkLabelURL           '(| :AngleLinkLabelURI :LinkURI)
-                :AngleLinkLabelURI  ["<" :LinkURI ">"]
-            :SingleLineLLDTitle     :LLDTitle
-            :DblLineLLDTitle        [#"[ \t]+\n[ \t]+" :LLDTitle]
-                :LLDTitle           '(| :DblQuoteLLDT :SnglQuoteLLDT :ParenLLDT)
-                    :DblQuoteLLDT   ["\""   :LinkTitle "\""]
-                    :SnglQuoteLLDT  ["'"    :LinkTitle "'"]
-                    :ParenLLDT      ["("    :LinkTitle ")"]})
+        :BlankLine                  [:Enter #"^[ \t]*" :Enter]
+    :Enter      (System/getProperty "line.separator")
+    })
+
+
+
+;(def grammar {
+;    :Start                          :Document
+;    :Enter                          (System/getProperty "line.separator")
+;    :Line                           ['(+ (| :EscapedChar :Span #"^.")) '(| :Enter :$)]
+;    :Document                       ['(+ (| :BlankLine :Block :Line)) :$]
+;    :EscapedChar                    ["\\" :EscapableChar]
+;        :EscapableChar              '(| "\\" "`" "*" "_" "{" "}" "[" "]" "(" ")" "#" "+" "-" "." "!")
+;    :Span                           '(| :Link :LiteralAsterisk :LiteralUnderscore :Emphasis :Code :Image :AutomaticLink)
+;        :LiteralAsterisk            "* "
+;        :LiteralUnderscore          "_ "
+;        :Link                       ["[" :LinkTextOrLabel "]" '(| :InlineLink :ReferenceLink)]
+;            :LinkTextOrLabel        #"^[^]]+"
+;            :InlineLink             ["(" :LinkURL '(? [" " "\"" :LinkTitle "\""]) ")"]
+;            :ReferenceLink          '(| :ImplicitRefLink ['(? " ") "[" :LinkLabel "]"])
+;               :ImplicitRefLink     ['(? " ") "[]"]
+;        :Emphasis                   '(| :StrongEmphasis :EMEmphasis)
+;            :StrongEmphasis         '(| :StrongEmphasis** :StrongEmphasis__)
+;                :StrongEmphasis**   ["**" '(+ [(! "**") #"^(?s)."]) "**"]
+;                :StrongEmphasis__   ["__" '(+ [(! "__") #"^(?s)."]) "__"]
+;            :EMEmphasis             '(| :EMEmphasis* :EMEmphasis_)
+;                :EMEmphasis*        ["*" #"^[^*]+" "*"]
+;                :EMEmphasis_        ["_" #"^[^_]+" "_"]
+;        :Code                       '(| :SingleBacktick :DoubleBacktick :SpacedBacktick)
+;            :SingleBacktick         ["`" '(? " ") #"^[^`]+" '(? " ") "`"]
+;            :DoubleBacktick         ["``" '(? " ") '(+ [(! "``") #"^(?s)."]) '(? " ") "``"]
+;        :Image                      ["!" :Link]
+;        :AutomaticLink              ["<" :LinkText ">"]
+;            :LinkText               #"^[^>]+"
+;    :Block                          '(| :Paragraph :Header :Blockquote :List :Codeblock :HRule :LinkLabelDefinition)
+;        :Paragraph                  ['(+ :LineOfText) '(& (| :BlankLine :$))]
+;            :BlankLine              [#"^[ \t]*$" :Enter]
+;            :LineOfText             '(| :BreakEndedLine :Line)
+;                :BreakEndedLine     ['(& #"^.+[ ][ ]+$") :Line]
+;        :Header                     '(| :SetextHeader :ATXHeader)
+;            :SetextHeader           [:HeaderText :Enter '(| (+ "=") (+ "-"))]
+;            :ATXHeader              '(| :ATXHeader1 :ATXHeader2 :ATXHeader3 :ATXHeader4 :ATXHeader5 :ATXHeader6)
+;                :ATXHeader1         ["#"        :HeaderText '(* "#")]
+;                :ATXHeader2         ["##"       :HeaderText '(* "#")]
+;                :ATXHeader3         ["###"      :HeaderText '(* "#")]
+;                :ATXHeader4         ["####"     :HeaderText '(* "#")]
+;                :ATXHeader5         ["#####"    :HeaderText '(* "#")]
+;                :ATXHeader6         ["######"   :HeaderText '(* "#")]
+;        :Blockquote                 [:BlockquoteFirstLine '(* (| :BlockquoteFirstLine :Line))]
+;            :BlockquoteFirstLine    ['(+ "> ") :Line]
+;        :List                       '(| :OrderedList :UnorderedList)
+;            :OrderedList            '(+ [:OrderedMarker     :ListLine])
+;            :UnorderedList          '(+ [:UnorderedMarker   :ListLine])
+;                :OrderedMarker      [:ListIndent #"^\d+\."          :ListSpace]
+;                :UnorderedMarker    [:ListIndent '(| "*" "+" "-")   :ListSpace]
+;                :ListIndent         #"^[ ]{0,3}"
+;                :ListSpace          '(| #"^[ ]+" "\t")
+;                :ListLine           :Line
+;        :Codeblock                  [:CodeIndent :Line]
+;            :CodeIndent             '(| "    " "\t")
+;        :HRule                      '(| #"^(-|- ){3,}" #"^(\*|\* ){3,}" #"^(_|_ ){3,}")
+;        :LinkLabelDefinition        [#"^[ ]{0,3}" "[" :LinkLabel "]:" #"^[ \t]+" :LinkLabelURL '(? (| :SingleLineLLDTitle :DblLineLLDTitle))]
+;            :LinkLabel              #"^[^]]+"
+;            :LinkLabelURL           '(| :AngleLinkLabelURI :LinkURI)
+;                :AngleLinkLabelURI  ["<" :LinkURI ">"]
+;            :SingleLineLLDTitle     :LLDTitle
+;            :DblLineLLDTitle        [#"[ \t]+\n[ \t]+" :LLDTitle]
+;                :LLDTitle           '(| :DblQuoteLLDT :SnglQuoteLLDT :ParenLLDT)
+;                    :DblQuoteLLDT   ["\""   :LinkTitle "\""]
+;                    :SnglQuoteLLDT  ["'"    :LinkTitle "'"]
+;                    :ParenLLDT      ["("    :LinkTitle ")"]})
--- test/com/lithinos/amotoen/test/core.clj
+++ test/com/lithinos/amotoen/test/core.clj
@@ -6,22 +6,47 @@
   (:use [com.lithinos.amotoen.string-wrapper]
         [com.lithinos.amotoen [markdown :rename {grammar markdown-grammar}]]))
 
-(deftest standard-test-files
-    (let [mdp (create-parser markdown-grammar :debug)]
-        (pprint (mdp (wrap-string "
-
-")))    
-#_        (pprint (mdp (wrap-string "a")))
-#_        (pprint (mdp (wrap-string "a
-")))
-#_        (pprint (mdp (wrap-string "a
-
-")))
-#_        (pprint (mdp (wrap-string "a
-
-a")))
-#_        (doseq [f (list (first (filter #(re-find #"\.text$" (.getName %)) (file-seq (File. "./test/MarkdownTests")))))]
+#_(deftest standard-test-files
+    (let [mdp (create-parser markdown-grammar)]
+        (doseq [f (list (first (filter #(re-find #"\.text$" (.getName %)) (file-seq (File. "./test/MarkdownTests")))))]
             (println f)
             (let [correct (File. (.replaceAll (.getCanonicalPath f) ".text$" ".html"))]
                 (is (= (mdp (wrap-string (slurp f))) (slurp correct)))))
                 ))
+
+(deftest double-newlines
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "
+
+"))]
+        (pprint result)))
+
+(deftest single-regularchar
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "a"))]))
+
+(deftest regularchar-newline
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "a
+"))]))
+
+(deftest regularchar-double-newline
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "a
+
+"))]
+        (pprint result)))
+
+(deftest regularchar-double-newline-regularchar
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "a
+
+a"))]
+        (pprint result)))
+
+(deftest regularchar-double-newline-with-whitespace-regularchar
+    (let [mdp       (create-parser markdown-grammar)
+          result    (mdp (wrap-string "a
+                
+a"))]
+        (pprint result)))
