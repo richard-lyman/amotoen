5ad3bf87aef4ba635633254cf95707bcf289b47f
Richard Lyman <richard.lyman@gmail.com>
2011-12-27 22:00:05 -0700

    .

--- src/com/lithinos/amotoen/core.clj
+++ src/com/lithinos/amotoen/core.clj
@@ -11,12 +11,12 @@
 
 (declare pegasus)
 (defprotocol IPosition
-    (psdebug [t] "")
+    (psdebug [t] "Some form of helpful debug info")
     (clone [t] "")
     (gp [t] "Returns pos") ; E.V.I.L.
     (sp [t j] "Sets pos") ; E.V.I.L.
-    (in [t] "")
-    (de [t] "")
+    (in [t] "Indent")
+    (de [t] "Dedent")
     (m [t] "Returns the 'c' then (inc pos)")
     (c [t] "The character at pos"))
 (defn gen-ps
@@ -43,8 +43,7 @@
 (defn pegs [s] (vec (seq s)))
 
 
-(defn- debug [w & args] #_(print (psdebug w)) #_(apply println args))
-(defn- debugt [w & args] #_(print (psdebug w)) #_(apply println args))
+(defn- debug [w & args] #_(do (print (psdebug w)) (apply println args) (flush)))
 
 (def #^{:private true} grammar-grammar {
     :Whitespace     '(| \space \newline \tab \,)
@@ -67,43 +66,29 @@
     :ValidKeywordChar (lpegs '| "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789:/*+!_?-")
 })
 
+; This needs to be some form of pmap
 (defn- either [n g w]
     (let [original (gp w)] ; E.V.I.L.
         (first (keep  #(do  (sp w original) ; E.V.I.L.
-                            ;(debug w "Either trying:" (pr-str %))
                             (pegasus % g w))
                         (rest n)))))
 
 (defn- type-list [n g w]
     (let [t (first n)
-          b (second n)
-          result (cond
-                    (= t '|) (let [temp (either n g w)] (debug w "Either returning:" temp) temp)
-                    (= t '*) (list
-                                (doall
-                                    (take-while
-                                        #(cond
-                                            (keyword? b)(b %)
-                                            (list? b)   (do (debug w "Zero-or-more on list:" %) %)
-                                            (vector? b) %
-                                            (char? b)   %
-                                            true        (throw (Error. (str "Not supposed to get here: " %))))
-                                        (repeatedly #(pegasus b g w)))))
-                    (= t '?) (pegasus b g w)
-                    (= t '%) (let [c    (c w)
-                                   temp (pegasus b g w)]
-                                ; If we succeed, then we fail - that's the point of AnyNot... If we fail, then we accept the current char
-                                (if temp nil (do (debug w "AnyNot MATCH:" (pr-str b)) (m w) c) )))]
-        result))
+          b (second n)]
+        (cond   (= t '|) (let [temp (either n g w)] (debug w "Either returning:" temp) temp)
+                (= t '*) (list (doall (take-while   #(if (keyword? b) (b %) %) ; When it's not keyword?, it's list? or vector? or char?
+                                                    (repeatedly #(pegasus b g w)))))
+                (= t '?) (pegasus b g w)
+                            ; If we succeed, then we fail - that's the point of AnyNot... If we fail, then we accept the current char
+                (= t '%) (let [c (c w)] (if (pegasus b g w) nil (do (debug w "AnyNot MATCH:" (pr-str b)) (m w) c) )))))
 
 (defn- try-char [n w]
     (if (= n (c w))
         (do
             (debug w (str "MATCH: '" (pr-str n) "' with '" (c w) "'"))
             (m w))
-        (do
-            ;(debug w (str "Char mismatch: '" (pr-str n) "' with '" (c w) "'"))
-            nil)))
+        nil))
 
 (defn- peg-vec [n g w]
     (loop [remaining    n
@@ -116,24 +101,22 @@
                             (conj result temp))
                     nil)))))
 
-(defn- p [w s n] (debug w s (pr-str n)) (flush))
+(defn- p [w s n] (debug w s (pr-str n)))
 
 (defn pegasus [n g w]
     (in w)
-    (let [result
-    (cond
-        (keyword? n)(do (p w "k:" n) (flush) (let [temp (pegasus (n g) g w)] (if temp {n temp} nil)))
-        (vector? n) (do #_(p w "v:" n) (flush) (peg-vec n g w))
-        (list? n)   (do #_(p w "l:" n) (flush) (type-list n g w))
-        (char? n)   (do #_(p w "c:" n) (flush) (try-char n w))
-        true        (throw (Error. (str "Unknown type: " n))))]
+    (let [result (cond
+                    (keyword? n)(do (p w "k:" n)    (let [temp (pegasus (n g) g w)] (if temp {n temp} nil)))
+                    (vector? n) (do #_(p w "v:" n)  (peg-vec n g w))
+                    (list? n)   (do #_(p w "l:" n)  (type-list n g w))
+                    (char? n)   (do #_(p w "c:" n)  (try-char n w))
+                    true        (throw (Error. (str "Unknown type: " n))))]
     (de w)
     result))
 
 (defn self-check []
     #_(println (pr-str (pegasus :Grammar grammar-grammar (gen-ps (pr-str {:S \a}))))) (flush)
     #_(println (pr-str (pegasus :Grammar grammar-grammar (gen-ps "{:S \\a}")))) (flush)
-    ; Shouldn't be nil
     (pprint (pegasus :Grammar grammar-grammar (gen-ps (pr-str grammar-grammar)))) (flush)
     #_(println (pr-str (pegasus :Grammar grammar-grammar (gen-ps "{:S \\a :B \\b}")))) (flush)
     #_(println (pr-str (pegasus :Grammar grammar-grammar (gen-ps (pr-str
@@ -145,9 +128,7 @@
         ))))) (flush)
     )
 
-(self-check)
-
-
+#_(self-check)
 
 #_(
 {:Grammar
