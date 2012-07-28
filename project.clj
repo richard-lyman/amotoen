1e5bf805a812ea4a27b55ad45658092d79defaca
Richard Lyman <richard.lyman@gmail.com>
2012-07-28 14:39:56 -0600

    Tweaks to the README as well as bumping some of the project.clj to use the new lein2.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
     :url "http://www.lithinos.com/amotoen"
     :license {:name "EPL-v1.0" :distribution :repo :comments "same as Clojure" :url "http://www.eclipse.org/legal/epl-v10.html"}
     :source-path "src"
-    :resources-path "res"
-    :dependencies [[org.clojure/clojure "1.3.0"]]
+    :resource-paths ["res"]
+    :dependencies [[org.clojure/clojure "1.4.0"]]
     :jar-name "amotoen.jar"
-    :jar-exclusions [#"(markdown|minimark|csv|json|project).clj" #"maven"]
+    :jar-exclusions [#"(markdown|minimark|project).clj" #"maven"]
     :manifest {"Built-By" "\"Richard Lyman\" <richard.lyman@gmail.com>"})
